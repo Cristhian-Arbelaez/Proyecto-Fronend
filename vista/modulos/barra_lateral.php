6747dc402df4e91ca2e2940ad83f130e2dc67048
Cristhian Arbelaez <carbelaez@unicauca.edu.co>
2023-12-02 12:47:22 -0500

    Proyecto Fronend V-5

--- vista/modulos/barra_lateral.php
+++ vista/modulos/barra_lateral.php
@@ -2,9 +2,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="" class="brand-link">
-    <img src="vista/css/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <img src="vista/css/dist/img/AP3.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
       style="opacity: .8">
-      <span class="brand-text font-weight-light"> Productos </span>
+      <span class="brand-text font-weight-light"> A.P. | Inventario </span>
   </a>
 
   <!-- Sidebar -->
--- vista/plantilla.php
+++ vista/plantilla.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>A.P. | Listas de Compras</title>
+  <title>A.P. | Inventario</title>
 
   <link rel="shortcut icon" href="vista/css/dist/img/AdminLTELogo.png" type="image/x-icon">
 
